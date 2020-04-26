716e7bb5c5f7c96a52ef5768e5761b9395c0f3f4
thomasslloyd <tom.lloydns@gmail.com>
2020-04-26 21:07:07 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EnvironmentMonitoring
+
+This project aims to create a simple, open source environment optimisation tool for improved well-being. Temperature and humidity conditions directly impact bodily function. This system acts to recommend changes to improve environment for optimal bodily function and optimal feel.
--- sensor_read/sensor_read.ino
+++ sensor_read/sensor_read.ino
@@ -1,36 +1,51 @@
+//Author: Thomas Lloyd
+
+
 # include <Arduino_HTS221.h>
+# include <WStream.h>
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600)
   HTS.begin()
-  float temperature
-  float humdity
-  
 
+  if (!HTS.begin()) {
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    Serial.println("Trying again!");
+    while(1);
+  }
+  
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  temperature, humidity = HTSRead(float temperature float humidity)
   
-  if (!HTS.begin()) {
-    Serial.println("Failed to initialize humidity temperature sensor!");
-    Serial.println("Trying again!");
-    while(1)
-  }
-
 }
 
-float HTSRead(float temperature float humidity) {
+float HTSRead() {
+
+  float temperature = HTS.readTemperature();
+  float humidity    = HTS.readHumidity();
+  
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" °C");
+
+  Serial.print("Humidity = ");
+  Serial.print(temperature);
+  Serial.println(" %");
+
+  // print an empty line
+  Serial.println();
+
+  // wait 1 second to print again
+  delay(1000);
+  
   return temperature, humidity 
 }
 
 
-
-
 void endGTS() {
-  HTS.end()
+  HTS.end();
 }
